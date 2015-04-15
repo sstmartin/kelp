91231fa7c44f23db857c52147bca4a0fd389016a
Scott St. Martin <sjsm3@gatech.edu>
2015-04-14 23:04:32 -0400

    made static work

--- cloud_kelp/settings.py
+++ cloud_kelp/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    '~/kelp/cloud_kelp/templates/',
+    '/Users/scottstmartin/Documents/development/Django/kelp/website/templates/',
 )
 
 # List of finder classes that know how to find static files in
